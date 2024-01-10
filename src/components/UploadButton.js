93f78aba51c8cc0f4c8af0391c75dce76c5c4965
Sehaj Naangal <snaangal@uwo.ca>
2024-01-09 23:41:13 -0500

    seperate upload button and upload file text box

--- src/components/UploadButton.js
+++ src/components/UploadButton.js
@@ -1,11 +1,12 @@
 // src/components/UploadButtonComponent.js
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { FileUpload } from "primereact/fileupload";
 import { Toast } from "primereact/toast";
 
 const UploadButtonComponent = ({ onFileUpload }) => {
     const toast = useRef(null);
     let file = undefined;
+    const [fileUploaded, setFileUploaded] = useState(false);
 
     // const saveFileLocally = (file) => {
     //     return new Promise((resolve, reject) => {
@@ -37,6 +38,7 @@ const UploadButtonComponent = ({ onFileUpload }) => {
         // Save file locally if you need it in a url e.x. localhost:8000/{uuid}
         // url = await saveFileLocally(file);
         toast.current.show({ severity: "success", summary: "Success", detail: "Uploaded " + file.name, life: 2000 });
+        setFileUploaded(true);
     }
 
     const onError = (event) => {
@@ -44,30 +46,49 @@ const UploadButtonComponent = ({ onFileUpload }) => {
         file = undefined;
     };
 
+    const uploadButtonContainerStyles = {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        width: "30%",
+    }
+
+    const uploadFileTextStyles = {
+        width: "100%",
+        height: "40px",
+        backgroundColor: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "1.3rem",
+        marginRight: "10px"
+    }
+
     const uploadButtonStyles = {
-        width: "50%", 
-        padding: "20px", 
-        backgroundColor: "#f8f9fa"
+        width: "150px",
+        height: "40px",
+        backgroundColor: "#cbb593",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center"
     }
 
     return (
-        <div style={uploadButtonStyles}>
-            <Toast ref={toast} />
-            <FileUpload
-                name="files[]"
-                mode="basic"
-                onSelect={onSelect}
-                onError={onError}
-                multiple={false}
-                accept=".mp3,.wav,.mid,.midi"
-                style={{
-                    width: "100%",
-                    backgroundColor: "#007bff",
-                    color: "#fff",
-                    padding: "10px",
-                    borderRadius: "5px",
-                }}
-            />
+        <div style={uploadButtonContainerStyles}>
+            <p style={uploadFileTextStyles}>Upload File</p>
+            <div style={uploadButtonStyles}>
+                <Toast ref={toast} />
+                <FileUpload
+                    name="files[]"
+                    mode="basic"
+                    onSelect={onSelect}
+                    onError={onError}
+                    multiple={false}
+                    accept=".mp3,.wav,.mid,.midi"
+                    chooseLabel="Upload"
+                />
+            </div>
         </div>
     );
 };
