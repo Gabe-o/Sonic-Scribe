3c92e2ff0ae7f37edd32aa7364ecb993b0f00087
ljeyaran1 <ljeyaran@uwo.ca>
2024-03-11 17:53:55 -0400

    init

---
+++ Frontend/src/components/SearchPage/SearchPage.jsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { useState, useEffect } from "react";
+import NavigationBar from "../NavigationBar/NavigationBar";
+
+const SearchPage = () => {
+    
+    return (
+        <div className="Header">
+            <NavigationBar />
+
+            
+
+        </div>
+    );
+
+
+
+
+
+};
