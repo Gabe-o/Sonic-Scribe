ab2ad281c6d26d96a6e890a528f7aa0c4e2ef2d6
Hersh-SS <hersheysandhu@gmail.com>
2024-03-18 21:08:03 -0400

    Updated account upload page

--- Frontend/src/components/AccountUpload/AccountUpload.jsx
+++ Frontend/src/components/AccountUpload/AccountUpload.jsx
@@ -1,18 +1,37 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useRef } from 'react';
 import { UserContext } from '../../contexts/user';
 
 // styles
 import './AccountUpload.css';
 
-const AccountUpload = () => {
-    const { user } = useContext(UserContext);
-    useEffect(() => {}, [user]);
+const AccountUpload = ({ onFileSelected }) => {
+  const fileInputRef = useRef(null);
 
-    return (
-        <>
-          <p>Upload Page</p>  
-        </>
-    );
-}
+  const handleUploadClick = () => {
+      fileInputRef.current.click(); // Trigger the hidden file input click
+  };
+
+  const handleFileChange = (event) => {
+      const file = event.target.files[0];
+      if (file) {
+          onFileSelected(file); // Call the passed in callback function with the selected file
+      }
+  };
+
+  return (
+      <>
+          <input 
+              type="file" 
+              ref={fileInputRef}
+              onChange={handleFileChange} 
+              accept=".mp3,.wav,.mid,.midi" 
+              style={{ display: 'none' }} 
+          />
+          <div className='upload-button' onClick={handleUploadClick}>
+              + Upload File
+          </div>
+      </>
+  );
+};
 
 export default AccountUpload;
